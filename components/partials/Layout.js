e4e120a6faa81ceaf8e0fd355af0d081f9c85b88
andmcdonald1 <103446633+andmcdonald1@users.noreply.github.com>
2022-08-16 15:09:57 -0700

    setting up workflow

---
+++ components/partials/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Title } from "react-native-paper";
+
+const Layout = ({}) => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+});
